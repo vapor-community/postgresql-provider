8015571160dcb43c8efbc847f05d29173b8e1095
Nate Bird <natebird@gmail.com>
2016-09-30 16:28:01 -0400

    Internal vapor components should rely on majorVersions only.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporPostgreSQL",
     dependencies: [
-   		 .Package(url: "https://github.com/vapor/postgresql-driver.git", majorVersion: 1, minor: 0),
+   		 .Package(url: "https://github.com/vapor/postgresql-driver.git", majorVersion: 1),
    		 .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0)
     ]
 )
