030e7aca103094904dff566fcd5a10db3b997065
Nate Bird <natebird@gmail.com>
2016-10-21 08:36:02 -0400

    Fix indentation

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporPostgreSQL",
     dependencies: [
-   		 .Package(url: "https://github.com/vapor/postgresql-driver.git", majorVersion: 1),
-         .Package(url: "https://github.com/vapor/vapor.git", versions: Version(1,1,0)..<Version(2,0,0)),
+        .Package(url: "https://github.com/vapor/postgresql-driver.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/vapor.git", versions: Version(1,1,0)..<Version(2,0,0)),
     ]
 )
