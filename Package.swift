558a0090e7db81f6ed37a542e5c8a4365c2fb3ae
pruthvikar <pruthvikar@gmail.com>
2017-05-17 20:14:00 +0200

    Update Package.swift
    Pinning using major version

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ let package = Package(
         // PostgreSQL driver for Fluent
         .Package(url: "https://github.com/vapor/postgresql-driver.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
         // A provider for including Fluent in Vapor applications
-        .Package(url: "https://github.com/vapor/fluent-provider.git", Version(1,0,0)),
+        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
         // A web framework and server for Swift that works on macOS and Ubuntu.
-        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0)),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
     ]
 )
