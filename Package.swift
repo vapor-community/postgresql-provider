e0f3d8738ef505e7f693cd45fd475735d799b445
Zsolt Váradi <vzsg@users.noreply.github.com>
2017-05-18 00:32:31 +0200

    Trigger CI

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
         // A provider for including Fluent in Vapor applications
         .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
         // A web framework and server for Swift that works on macOS and Ubuntu.
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
     ]
 )
