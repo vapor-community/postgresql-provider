89cb3159f208ee6a2c241c135ebb069191fb6fbc
pruthvikar <pruthvikar@gmail.com>
2017-05-18 02:22:29 +0200

    Update Package.swift (#23)
    * Update Package.swift
    
    Update Package.swift to use Vapor 2 release dependencies
    
    * Update Package.swift
    
    Pinning using major version
    
    * Update Package.swift
    
    Updated postgresql to major version 2
    
    * Trigger CI

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@ let package = Package(
     name: "PostgreSQLProvider",
     dependencies: [
         // PostgreSQL driver for Fluent
-        .Package(url: "https://github.com/vapor/postgresql-driver.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/vapor/postgresql-driver.git", majorVersion: 2),
         // A provider for including Fluent in Vapor applications
-        .Package(url: "https://github.com/vapor/fluent-provider.git", Version(1,0,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
         // A web framework and server for Swift that works on macOS and Ubuntu.
-        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
     ]
 )
